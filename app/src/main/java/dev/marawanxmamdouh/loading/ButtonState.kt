ac876ff52fd503d30aec088787e57e89a99a8ecb
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-22 23:49:02 +0200

    Create layout and add resources

---
+++ app/src/main/java/dev/marawanxmamdouh/loading/ButtonState.kt
@@ -0,0 +1,7 @@
+package dev.marawanxmamdouh.loading
+
+sealed class ButtonState {
+    object Clicked : ButtonState()
+    object Loading : ButtonState()
+    object Completed : ButtonState()
+}
---
+++ app/src/main/java/dev/marawanxmamdouh/loading/DetailActivity.kt
@@ -0,0 +1,16 @@
+package dev.marawanxmamdouh.loading
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
