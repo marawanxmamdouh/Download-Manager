35d08789387cbb6210660865f489d61c0e547a82
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-24 00:19:55 +0200

    Draw button start state

--- app/src/main/java/dev/marawanxmamdouh/loading/LoadingButton.kt
+++ app/src/main/java/dev/marawanxmamdouh/loading/LoadingButton.kt
@@ -1,22 +1,29 @@
 package dev.marawanxmamdouh.loading
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
 
+
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
+    private var widthSize = 0f
+    private var heightSize = 0f
 
-    private val valueAnimator = ValueAnimator()
+    private var buttonState: ButtonState by Delegates.observable(ButtonState.Completed) { p, old, new ->
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.LEFT
+        textSize = resources.getDimension(R.dimen.default_text_size)
+        typeface = Typeface.create("", Typeface.BOLD)
     }
 
 
@@ -25,22 +32,33 @@ class LoadingButton @JvmOverloads constructor(
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        canvas.drawColor(resources.getColor(R.color.colorPrimary))
+        drawStartState(canvas)
+    }
 
+    private fun drawStartState(canvas: Canvas) {
+        paint.color = resources.getColor(R.color.white)
+        canvas.drawText(
+            resources.getString(R.string.button_name),
+            widthSize / 2 - paint.measureText(resources.getString(R.string.button_name)) / 2,
+            heightSize / 2 + paint.descent(),
+            paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
         val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
+            View.MeasureSpec.getSize(w),
             heightMeasureSpec,
             0
         )
-        widthSize = w
-        heightSize = h
+        widthSize = w.toFloat()
+        heightSize = h.toFloat()
+
         setMeasuredDimension(w, h)
     }
-
 }
