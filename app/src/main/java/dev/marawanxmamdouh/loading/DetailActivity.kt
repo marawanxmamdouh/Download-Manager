a9c1280770210bdbb8089ed895459903c651a1ba
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-30 00:30:06 +0200

    get the data in the detail Activity

--- app/src/main/java/dev/marawanxmamdouh/loading/DetailActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/loading/DetailActivity.kt
@@ -1,16 +1,45 @@
 package dev.marawanxmamdouh.loading
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import dev.marawanxmamdouh.loading.databinding.ContentDetailBinding
+
+private const val TAG = "DetailActivity"
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ContentDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ContentDetailBinding.inflate(layoutInflater)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        var status = ""
+        var downloadName = ""
+
+        if (intent != null) {
+            Log.i(TAG, "onCreate (line 26): intent is not null")
+            status = intent.getStringExtra("status") ?: ""
+            downloadName = intent.getStringExtra("downloadName") ?: ""
+        }
+
+        Log.i(TAG, "onCreate (line 27): $status")
+        Log.i(TAG, "onCreate (line 28): $downloadName")
+
+        if (status == "Success") {
+            binding.downloadStatus.setTextColor(Color.GREEN)
+        } else {
+            binding.downloadStatus.setTextColor(Color.RED)
+        }
+        binding.downloadName.text = downloadName
+        binding.downloadStatus.text = status
+
+        setContentView(binding.root)
     }
 }
